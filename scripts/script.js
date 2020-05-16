7d89fe69c7588a64f3c27ad9e7f1bc02a7d50880
Stepan Matveev <stady30@yandex.ru>
2020-05-16 22:43:46 +0300

    Предрелиз

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,51 @@
+// Присваиваем необходимые переменные
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const submitButton = document.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+
 // Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
+const formElement = document.querySelector('.popup__form');
+// Берем значения из полей ввода формы
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_occupation');
+
+
+
+//Функция открытия\закрытия
+function openPopup () {
+    if (popup.classList.contains('popup_viev_open')) { 
+        popup.classList.remove('popup_viev_open');
+        popup.classList.add('popup_viev_close');
+    }  //проверяем открыт ли попап и закрываем если нужно
+    else {
+        popup.classList.remove('popup_viev_close');
+        popup.classList.add('popup_viev_open');
+        nameInput.value = profileName.textContent; 
+        jobInput.value = profileJob.textContent;
+    } // открываем если закрыт и берем значения для полей ввода из профиля
+}
+
+//Назначяем логику кнопок редактировать и закрыть
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', openPopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = // Воспользуйтесь инструментом .querySelector()
-    let jobInput = // Воспользуйтесь инструментом .querySelector()
-
-    // Получите значение полей из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    evt.preventDefault(); 
+    // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
+        profileName.textContent = nameInput.value;
+            if (profileName.textContent.length > 10){ 
+                profileName.style.fontSize = '34px'
+        }
+            profileJob.textContent = jobInput.value;
+        openPopup();
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
